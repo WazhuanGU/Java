9bf4f3f81c11b6e45b078d597261e6a55d09df7d
WazhuanGU <w1124049544@gmail.com>
2020-10-03 17:18:59 +0800

    first commit

---
+++ Test.java
@@ -0,0 +1,26 @@
+/**
+ * Test
+ */
+
+ import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Scanner Aagument = new Scanner(System.in);
+
+        int argu_1 = Aagument.nextInt();
+        int argu_2 = Aagument.nextInt();
+        int argu_3 = Aagument.nextInt();
+        if(argu_1!=argu_2)
+            System.out.println("not equal");
+        else if(argu_1!=argu_3)
+            System.out.println("not equal");
+        else if(argu_2!=argu_3)
+            System.out.println("not equal");
+        else 
+            System.out.println("equal");
+
+        Aagument.close();
+    }
+}
